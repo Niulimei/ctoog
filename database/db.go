0e55eae84a4444239ba7629dc291dc9e00232b71
韩绍文 <bolide2005@163.com>
2021-04-28 13:38:45 +0800

    数据库

---
+++ database/db.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
+)
+
+var DB *sqlx.DB
+
+var schema = `
+CREATE TABLE user (
+    id integer PRIMARY KEY autoincrement,
+    username varchar (50),
+    password_hash varchar (32)
+);
+
+CREATE TABLE task (
+    id integer PRIMARY KEY autoincrement,
+    pvob varchar (256),
+    component varchar (256),
+    cc_user varchar (256),
+    cc_password varchar (256),
+    git_url varchar (256),
+    git_user varchar (256),
+    git_password varchar (256),
+    git_repo varchar (256),
+    status varchar (16),
+    last_completed_time varchar (64)
+);
+
+CREATE TABLE match_info (
+    id integer PRIMARY KEY autoincrement,
+    task_id integer,
+    stream varchar (256),
+    git_branch varchar (256)
+);
+
+CREATE TABLE task_log (
+    id integer PRIMARY KEY autoincrement,
+    task_id integer,
+    status varchar (16),
+    start_time varchar (64),
+    end_time varchar (64),
+    duration integer ,
+)
+`
+
+func init() {
+	var err error
+	var isInitAlready bool
+	_, err = os.Stat("translator.db")    //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			isInitAlready = true
+		}
+	}
+	DB, err = sqlx.Connect("sqlite3", "file:translator.db?cache=private&mode=rwc")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if !isInitAlready {
+		DB.MustExec(schema)
+	}
+}
