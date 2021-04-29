64c9fd915724babe65e17f93dceb3792495b2cae
13062592077 <gitea@fake.local>
2021-04-29 13:58:26 +0800

    用户相关接口2

--- database/db.go
+++ database/db.go
@@ -54,6 +54,8 @@ CREATE TABLE worker (
     status varchar (16),
     task_count integer
 );
+
+INSERT INTO user (username,password) VALUES("admin", "21232f297a57a5a743894a0e4a801fc3");
 `
 
 type TaskModel struct {
--- restapi/users.go
+++ restapi/users.go
@@ -4,42 +4,86 @@ import (
 	"ctgb/database"
 	"ctgb/models"
 	"ctgb/restapi/operations"
+	"ctgb/utils"
 	"github.com/go-openapi/runtime/middleware"
 	"net/http"
 )
 
 func CreateUserHandler(params operations.CreateUserParams) middleware.Responder {
+	checkRet := checkPermission(params.Authorization)
+	if checkRet != nil {
+		return checkRet
+	}
 	var id int
 	row := database.DB.QueryRow("SELECT id FROM user WHERE username=?", params.UserInfo.Username)
 	err := row.Scan(&id)
-	if err != nil || id != 0 {
-		return middleware.Error(http.StatusInternalServerError, "User Already Exist")
+	if err == nil || id != 0 {
+		return operations.NewCreateUserInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusInternalServerError,
+			Message: "User Already Exist",
+		})
 	}
 	sqlStr := "INSERT INTO user (username,password) VALUES (?,?)"
 	ret := database.DB.MustExec(sqlStr, params.UserInfo.Username, params.UserInfo.Password)
 	_, err = ret.RowsAffected()
 	if err != nil {
-		return middleware.Error(http.StatusInternalServerError, "Sql Error")
+		return operations.NewCreateUserInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusInternalServerError,
+			Message: "Sql Error",
+		})
 	} else {
-		return middleware.Error(http.StatusCreated, "User Create Success")
+		return operations.NewCreateUserCreated().WithPayload(&models.OK{Message: "User Create Success"})
 	}
 }
 
+func checkPermission(token string) middleware.Responder {
+	username, valid := utils.Verify(token)
+	if !valid {
+		return operations.NewListUserInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusUnauthorized,
+			Message: "",
+		})
+	}
+	if username != "admin" {
+		return operations.NewListUserInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusForbidden,
+			Message: "",
+		})
+	}
+	return nil
+}
+
 func ListUsersHandler(param operations.ListUserParams) middleware.Responder {
-	rows, err := database.DB.Query("SELECT username FROM user")
+	checkRet := checkPermission(param.Authorization)
+	if checkRet != nil {
+		return checkRet
+	}
+	rows, err := database.DB.Query("SELECT count(1) over() AS total_rows,username FROM user ORDER BY id LIMIT ? OFFSET ?", param.Limit, param.Offset)
 	if err != nil {
-		return middleware.Error(http.StatusInternalServerError, "Sql Error")
+		return operations.NewListUserInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusInternalServerError,
+			Message: "Sql Error",
+		})
 	}
 	defer rows.Close()
-	var users []*models.UserModel
+	var users []*models.UserInfoModel
+	var count int64
 	for rows.Next() {
-		tmp := &models.UserModel{}
-		if err := rows.Scan(&tmp.Username); err != nil {
-			return middleware.Error(http.StatusInternalServerError, "Sql Error")
+		tmp := &models.UserInfoModel{}
+		if err := rows.Scan(&count, &tmp.Username); err != nil {
+			return operations.NewListUserInternalServerError().WithPayload(&models.ErrorModel{
+				Code:    http.StatusInternalServerError,
+				Message: "Sql Error",
+			})
 		}
 		users = append(users, tmp)
 	}
-	return middleware.Error(http.StatusCreated, users)
+	return operations.NewListUserOK().WithPayload(&models.UserPageInfoModel{
+		Count:    count,
+		Limit:    param.Limit,
+		Offset:   param.Offset,
+		UserInfo: users,
+	})
 }
 
 func LoginHandler(params operations.LoginParams) middleware.Responder {
@@ -47,11 +91,20 @@ func LoginHandler(params operations.LoginParams) middleware.Responder {
 	row := database.DB.QueryRow("SELECT password FROM user WHERE username=?", params.UserInfo.Username)
 	err := row.Scan(&passwordInDB)
 	if err != nil {
-		return middleware.Error(http.StatusInternalServerError, "")
+		return operations.NewLoginInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusInternalServerError,
+			Message: "User Does Not Exist",
+		})
 	}
 	if passwordInDB == params.UserInfo.Password {
-		return middleware.Error(http.StatusCreated, "LoginHandler Success")
+		token := utils.CreateJWT(params.UserInfo.Username)
+		return operations.NewLoginCreated().WithPayload(&models.Authorization{
+			Token: token,
+		})
 	} else {
-		return middleware.Error(http.StatusInternalServerError, "User Does Not Exist Or Wrong Password")
+		return operations.NewLoginInternalServerError().WithPayload(&models.ErrorModel{
+			Code:    http.StatusInternalServerError,
+			Message: "Wrong Password",
+		})
 	}
 }
