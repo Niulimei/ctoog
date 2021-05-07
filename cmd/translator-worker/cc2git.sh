ca653f87d4010d39e965c9d014bb3451a05f3ca4
guo <guoshuaijie@oschina.cn>
2021-05-07 16:25:41 +0800

    shell脚本调整：问题修复2

--- cmd/translator-worker/cc2git.sh
+++ cmd/translator-worker/cc2git.sh
@@ -3,23 +3,29 @@
 ######
 #脚本名称：cc2git.sh
 #作用：完成CC代码的拉取，git仓库的初始化，代码向git的推送
-#传参说明：共需7个参数，依次分别为：
-#pvob名称，component名称，stream名称，gitRepoURL，git目标分支代码，任务ID，是否保留空目录(是：true，否：false)
+#传参说明：共需9个参数，依次分别为：
+#pvob名称，component名称，stream名称，gitRepoURL，git目标分支代码，任务ID，是否保留空目录(是：true，否：false)，用户名，邮箱
 ######
 
 set -e
 
 ccTmpRootPath="/home/tmp/pvobs_view"
 gitTmpRootPath="/home/tmp/git"
+cp=/usr/bin/cp
 
 initGitRepo(){
   repoUrl=$1
   branchName=$2
   tmpGitDir=$3
+  username=$4
+  email=$5
   masterBranchName="origin/init_master"
   mkdir -p ${tmpGitDir}
   cd ${tmpGitDir}
   git init
+  git config user.name "${username}"
+  git config user.email "${email}"
+  git config push.default simple
   git remote add origin ${repoUrl}
   git fetch --all
   remoteBrList=$(git branch -r)
@@ -43,10 +49,10 @@ initGitRepo(){
   else
     git checkout -b init_master
     touch ./.init_master
-    git add .init_master
+    git add -A .init_master
     git commit -m "init master"
     rm -rf .init_master
-    git add .init_master
+    git add -A .init_master
     git commit -m "delete master init file"
     git push origin init_master
   fi
@@ -61,11 +67,13 @@ pullCCAndPush(){
   gitBranchName=$5
   taskID=$6
   containEmptyDir=$7
+  username=$8
+  email=$9
   combainNameAdapt=$(echo -n ${pvobName}_${streamName}_${componentName} | sed 's/\//_/g')
   local tmpGitDir="${gitTmpRootPath}/${combainNameAdapt}_${taskID}"
   local tmpCCDir="${ccTmpRootPath}/${combainNameAdapt}_${taskID}"
   local tmpCCDirExist=false
-  local tmpGitDir=false
+  local tmpGitDirExist=false
   if [[ -d ${tmpCCDir} ]]; then
     tmpCCDirExist=true
     cd ${tmpCCDir}
@@ -78,13 +86,14 @@ pullCCAndPush(){
   if [[ -d ${tmpGitDir} ]]; then
     tmpGitDirExist=true
   else
-    initGitRepo ${gitRepoUrl} ${gitBranchName} ${tmpGitDir}
+    initGitRepo ${gitRepoUrl} ${gitBranchName} ${tmpGitDir} ${username} ${email}
   fi
-  cp -rf ${tmpCCDir}/* ${tmpGitDir}/*
+  $cp -rf ${tmpCCDir}/* ${tmpGitDir}/
   if [[ ${containEmptyDir} == "true" ]]; then
     find ${tmpGitDir} -type d -empty -not -path "./.git/*" -exec touch {}/.gitkeep \;
   fi
-  git add .
+  cd ${tmpGitDir}
+  git add -A .
   if $tmpCCDirExist && $tmpGitDirExist; then
     git commit -m "import from cc,update commit $(date '+%Y%m%d%H%M%S')"
   else
@@ -105,8 +114,8 @@ postClean(){
 main(){
   mkdir -p ${ccTmpRootPath}
   mkdir -p ${gitTmpRootPath}
-  pullCCAndPush $1 $2 $3 $4 $5 $6 $7
+  pullCCAndPush $1 $2 $3 $4 $5 $6 $7 $8 $9
   #postClean
 }
 
-main $1 $2 $3 $4 $5 $6 $7
+main $1 $2 $3 $4 $5 $6 $7 $8 $9
