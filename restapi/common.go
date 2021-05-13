6dda893c8c05fb99356284c8699c0044273d8e41
guo <guoshuaijie@oschina.cn>
2021-05-13 10:37:57 +0800

    新增操作日志

--- restapi/common.go
+++ restapi/common.go
@@ -12,6 +12,7 @@ import (
 func CheckPermission(token string) middleware.Responder {
 	username, valid := utils.Verify(token)
 	if !valid {
+		utils.RecordLog(utils.Info, utils.Login, "", "user "+username+" Unauthorized", http.StatusUnauthorized)
 		return operations.NewListUserInternalServerError().WithPayload(&models.ErrorModel{
 			Code:    http.StatusUnauthorized,
 			Message: "",
@@ -19,6 +20,7 @@ func CheckPermission(token string) middleware.Responder {
 	}
 	userInfo := getUserInfo(username)
 	if userInfo.RoleID != int64(AdminRole) {
+		utils.RecordLog(utils.Info, utils.Login, "", "user "+username+" Forbidden", http.StatusForbidden)
 		return operations.NewListUserInternalServerError().WithPayload(&models.ErrorModel{
 			Code:    http.StatusForbidden,
 			Message: "",
