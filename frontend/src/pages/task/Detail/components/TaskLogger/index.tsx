d63deabd7d76c3a0b9dd0eac1005541491e8eab8
zbl91555 <zhubolin@oschina.com>
2021-05-14 11:25:31 +0800

    fix: fix bug

--- frontend/src/pages/task/Detail/components/TaskLogger/index.tsx
+++ frontend/src/pages/task/Detail/components/TaskLogger/index.tsx
@@ -11,38 +11,41 @@ interface IProps {
 }
 
 /** 日志刷新间隔 */
-const RefreshLogInterval = 2000;
+const RefreshLogInterval = 3000;
 
 const TaskLogger: React.FC<IProps> = ({ actionRef }) => {
   const timerRef = React.useRef<any>();
   const [logData, setLogData] = React.useState('');
   const [visible, toggleVisible] = useToggle(false);
+  const disposeTimeout = () => {
+    clearTimeout(timerRef.current);
+  };
 
   React.useImperativeHandle(actionRef, () => ({
     open: async (id, autoRefresh = false) => {
-      const setLogOutput = async () => {
+      const refreshLog = async () => {
         const { content } = await taskService.getLogOutput(id);
         setLogData(content);
+        if (autoRefresh) {
+          timerRef.current = setTimeout(async () => {
+            disposeTimeout();
+            await refreshLog();
+          }, RefreshLogInterval);
+        }
       };
-
-      await setLogOutput();
+      refreshLog();
       toggleVisible(true);
-      if (autoRefresh) {
-        timerRef.current = setInterval(() => {
-          setLogOutput();
-        }, RefreshLogInterval);
-      }
     },
   }));
 
   const closeModal = () => {
     setLogData('');
     toggleVisible(false);
-    clearInterval(timerRef.current);
+    disposeTimeout();
   };
 
   useUnmount(() => {
-    clearInterval(timerRef.current);
+    disposeTimeout();
   });
 
   return (
