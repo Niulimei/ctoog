923272b6f166f2bf13fb76417153cc1f2174300d
guo <guoshuaijie@oschina.cn>
2021-06-01 15:48:54 +0800

    [Tomato-Scrum-11325] 新增日志转储定时任务：每天一个日志文件，清理三天前的日志

--- restapi/common.go
+++ restapi/common.go
@@ -2,8 +2,14 @@ package restapi
 
 import (
 	"ctgb/utils"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func CheckPermission(r *http.Request) bool {
@@ -28,4 +34,49 @@ func IsExceptionURL(method, uri string) bool {
 		}
 	}
 	return false
+}
+
+func DumpLogFile(logFile string) {
+	for {
+		N := time.Now()
+		y, m, d := N.Date()
+		delay := time.Date(y, m, d+1, 0, 0, 0, 0, time.Local).Second() - N.Second()
+		//delay := time.Date(y, m, d, N.Hour(), N.Minute(), 0, 0, time.Local).Second() - N.Second()
+		time.Sleep(time.Second * time.Duration(delay))
+		log.Debug("start bak")
+		fs, err := ioutil.ReadDir(filepath.Dir(logFile))
+		if err != nil {
+			log.Debug(err)
+		}
+		for _, f := range fs {
+			if time.Now().Day()-f.ModTime().Day() > 3 {
+				//if time.Now().Minute()-f.ModTime().Minute() > 2 && filepath.Base(logFile) != f.Name() {
+				log.Debug("start clean")
+				err = os.RemoveAll(filepath.Join(filepath.Dir(logFile), f.Name()))
+				if err != nil {
+					log.Debug(err)
+				}
+			}
+		}
+		s, err := os.Stat(logFile)
+		if err != nil {
+			log.Debug(err)
+			continue
+		}
+		co, err := ioutil.ReadFile(logFile)
+		if err != nil {
+			log.Debug(err)
+			continue
+		}
+		err = ioutil.WriteFile(logFile, []byte(""), s.Mode())
+		if err != nil {
+			log.Debug(err)
+			continue
+		}
+		err = ioutil.WriteFile(logFile+"_"+N.Format("2006.01.02_15:04:05"), co, s.Mode())
+		if err != nil {
+			log.Debug(err)
+			continue
+		}
+	}
 }
