25923e46fe88561245cb58bf77b879dc06d06c71
guo <guoshuaijie@oschina.cn>
2021-06-21 16:21:45 +0800

    [Tomato-Scrum-11325] 创建任务时检查用户及仓库信息2

--- fileapi/import_plans.go
+++ fileapi/import_plans.go
@@ -56,6 +56,7 @@ func checkData(rows [][]string) (bool, string) {
 			CCPassword: ccPassword,
 			GitRepoURL: utils.ParseGitURL(gitUser, gitPassword, gitUrl),
 			ModelType:  taskType,
+			SvnURL:     "",
 		}
 		code, errRet := utils.DoCheckInfoReq(checkInfos)
 		if code != http.StatusOK {
