1ef26b7fa7216c6c021779ae809cb88b36921a50
liukaizhen <kaizhenlau@gmail.com>
2022-09-07 15:24:31 +0800

    任务列表：task表增加gitlab_group,gitlab_project,gitlab_token,gitee_token,gitee_project,gitee_group 6个字段

---
+++ sql/task_1.0.3.sql
@@ -0,0 +1,6 @@
+ALTER TABLE task ADD COLUMN gitlab_group varchar(256) DEFAULT '';
+ALTER TABLE task ADD COLUMN gitlab_project varchar(256) DEFAULT '';
+ALTER TABLE task ADD COLUMN gitlab_token varchar(256) DEFAULT '';
+ALTER TABLE task ADD COLUMN gitee_group varchar(256) DEFAULT '';
+ALTER TABLE task ADD COLUMN gitee_token varchar(256) DEFAULT '';
+ALTER TABLE task ADD COLUMN gitee_project varchar(256) DEFAULT '';
